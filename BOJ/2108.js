7ed1761ab6c4498421ed5447b2dea998e2ff0b79
Jangkyoungwoo <kyoungwoo990124@gmail.com>
2022-02-02 19:55:56 +0900

    sort

--- BOJ/2108.js
+++ BOJ/2108.js
@@ -11,24 +11,47 @@ for (let i = 0; i < N; i++) {
 solution(N, testArr);
 
 function solution(N, testArr) {
-  const testArrSort = testArr.sort((a, b) => a - b);
+  const sortedNumArr = testArr.sort((a, b) => a - b);
   const evg = () => {
     let result = 0;
     for (let i = 0; i < N; i++) {
       result += testArr[i];
     }
-    result = Math.round(result / 5);
+    result = Math.round(result / N);
     console.log(result);
   }
   const middleVal = () => {
     const middle = (N - 1) / 2;
-    console.log(testArrSort[middle]);
+    console.log(sortedNumArr[middle]);
   }
   const mode = () => {
-    
+    const numMap = {};
+    for (let num of sortedNumArr) {
+      if (numMap[num]) {
+        numMap[num] = numMap[num] + 1;
+      } else {
+        numMap[num] = 1;
+      }
+    }
+    let maxNum = Math.max.apply(null, Object.values(numMap));
+    let maxNumArr = [];
+    let maxNumResult = 0;
+    for (let numKey in numMap) {
+      if (numMap[numKey] === maxNum) {
+        maxNumArr.push(numKey);
+      }
+    }
+
+    if (maxNumArr.length > 1) {
+      maxNumArr = maxNumArr.sort((a, b) => a - b);
+      maxNumResult = maxNumArr[1];
+    } else {
+      maxNumResult = maxNumArr[0];
+    }
+    console.log(+maxNumResult);
   }
   const range = () => {
-    console.log(testArrSort[N - 1] - testArrSort[0]);
+    console.log(sortedNumArr[N - 1] - sortedNumArr[0]);
   }
   evg();
   middleVal();
